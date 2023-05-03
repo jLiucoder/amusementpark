9a0532d5b07e9836b77714ba8f1301f34cf0769d
lenoxcyy13 <lenoxcyy13@gmail.com>
2023-05-03 18:20:15 -0400

    discount done

--- ticket/models.py
+++ ticket/models.py
@@ -7,10 +7,10 @@ from visitor.models import JlsVisitors
 class JlsTickets(models.Model):
     tk_id = models.AutoField(primary_key=True, db_comment='Unique ticket ID')
     tk_method = models.CharField(max_length=2, db_comment='Ticket method, online OL or onsite OS')
-    tk_purdate = models.DateTimeField(db_comment='Purchase date of the ticket')
-    tk_vdate = models.DateTimeField(db_comment='Visit date of the ticket')
+    tk_purdate = models.DateTimeField(db_comment='Purchase datetime of the ticket')
+    tk_vdate = models.DateField(db_comment='Visit date of the ticket')
     tk_price = models.SmallIntegerField(db_comment='Ticket Price of the ticket')
-    tk_discount = models.IntegerField(db_comment='Percentage off of the ticket')
+    tk_discount = models.FloatField(db_comment='Percentage off of the ticket')
     v = models.ForeignKey(JlsVisitors, models.DO_NOTHING)
     invoi = models.ForeignKey(JlsInvoi, models.DO_NOTHING, null=True)
 
--- ticket/views.py
+++ ticket/views.py
@@ -1,55 +1,59 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.template import loader
 from .models import JlsTickets
-from django.shortcuts import render, redirect, reverse
-from datetime import datetime
+from visitor.models import JlsVisitors
+from django.shortcuts import render
+from datetime import datetime, date
 from django.contrib import messages
 from django.views import View
-from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+import holidays
 
-
-# @login_required(login_url='home')
-# def ticket(request):
-#     ticket = JlsTickets.objects.all().values()
-#     template = loader.get_template('ticket.html')
-#     context = {
-#         'ticket': ticket,
-#     }
-#     if request.method == 'POST':
-
-#         new_tk = JlsTickets()
-#         new_tk.tk_id = 1 # should be incremented
-#         new_tk.tk_method = 'OL'
-#         new_tk.tk_purdate = datetime.today()
-#         new_tk.tk_vdate = datetime.today()
-#         new_tk.tk_price = 60
-#         new_tk.tk_discount = 0
-#         new_tk.save()
-#         messages.success(request, 'Successfully added')
-#     return HttpResponse(template.render(context, request))
 
 class TicketView(LoginRequiredMixin, View):
     template_name = 'ticket.html'
-    # success_url = reverse_lazy('memberships')
+
+    def calculateAge(self, birthDate):
+        today = date.today()
+        age = today.year - birthDate.year -((today.month, today.day) <(birthDate.month, birthDate.day))
+        return age
+    
+    def calculateDiscount(self, method, vdate, visitor):
+        discount = 0
+        age = self.calculateAge(visitor.v_dob)
+        print(age)
+        us_holidays = holidays.US()
+        if vdate in us_holidays:
+            discount = 0
+        else:
+            if method == 'OL':
+                discount += 0.05
+            if visitor.v_type == 'M':
+                discount += 0.1
+            if age >= 60 or age < 7:
+                discount += 0.15
+        return discount
 
     def get(self, request):
         return render(request, self.template_name)
 
     def post(self, request, **kwargs):
         current_user_id = self.request.user.id
-        # new_tk = JlsTickets.objects.filter(v_id=current_user_id).first()
-        new_tk = JlsTickets()
-        new_tk.v_id = current_user_id
-        new_tk.tk_id = 1 # should be incremented
-        new_tk.tk_method = 'OL'
-        new_tk.tk_purdate = datetime.today()
-        new_tk.tk_vdate = datetime.today()
-        new_tk.tk_price = 60
-        new_tk.tk_discount = 0
-        new_tk.save()
-        messages.success(request, 'Successfully added')
-        # visitor.refresh_from_db()
-        # return redirect(self.success_url)
+        vdate = request.POST.get('visit date')
+        if vdate == "":
+            messages.success(request, "Please select a date to visit")
+        else:
+            tk = JlsTickets.objects.filter(v_id=current_user_id, tk_vdate=vdate)
+            if len(tk) != 0:
+                messages.success(request, "You've already bought a ticket!")
+            else: 
+                visitor = JlsVisitors.objects.get(user_id=current_user_id)
+
+                new_tk = JlsTickets()
+                new_tk.v_id = current_user_id
+                new_tk.tk_method = 'OL'
+                new_tk.tk_purdate = datetime.today()
+                new_tk.tk_vdate = vdate
+                new_tk.tk_price = 60
+                new_tk.tk_discount = self.calculateDiscount(new_tk.tk_method, vdate, visitor)
+                new_tk.save()
+                messages.success(request, 'Successfully added')
         return render(request, self.template_name)
