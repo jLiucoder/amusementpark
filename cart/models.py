70e07c04f3b5f7e5a90f70f75a2bd920b803c654
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2023-05-03 14:09:00 -0400

    working version of member and groups

--- cart/models.py
+++ cart/models.py
@@ -62,12 +62,10 @@ from django.db import models
 class JlsInvoi(models.Model):
     invoi_id = models.IntegerField(primary_key=True, db_comment='Unique Invoice ID')
     invoi_date = models.DateTimeField(db_comment='Date for the corresponding invoice')
-    # invoi_quant = models.DecimalField(max_digits=4, decimal_places=2, db_comment='Amount for the invoice')
     invoi_amount = models.IntegerField(db_comment='Invoice amount (how much money). ')
     invoi_type = models.CharField(max_length=20, db_comment='Invoice type. Type can be "Tickets, Shows, Stores, Parkings". ')
 
     class Meta:
-
         db_table = 'jls_invoi'
 
 
