967c4dc881e1f804c6a1d82ef98756cf246b1f63
lenoxcyy13 <lenoxcyy13@gmail.com>
2023-05-06 20:00:24 -0400

    ticket add delete function

--- ticket/models.py
+++ ticket/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from cart.models import JlsInvoi
 from visitor.models import JlsVisitors
 
-
 # Create your models here.
 
 class JlsTickets(models.Model):
@@ -12,8 +11,9 @@ class JlsTickets(models.Model):
     tk_vdate = models.DateField(db_comment='Visit date of the ticket')
     tk_price = models.SmallIntegerField(db_comment='Ticket Price of the ticket')
     tk_discount = models.FloatField(db_comment='Percentage off of the ticket')
+    invoi = models.ForeignKey(JlsInvoi, on_delete=models.CASCADE, null=True)
     v = models.ForeignKey(JlsVisitors, models.DO_NOTHING)
-    invoi = models.ForeignKey(JlsInvoi, models.DO_NOTHING, null=True)
 
     class Meta:
+        managed = True
         db_table = 'jls_tickets'
--- ticket/views.py
+++ ticket/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.template import loader
 from django.contrib import messages
+from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 
@@ -43,41 +45,55 @@ class TicketView(LoginRequiredMixin, View):
         return price*(1-discount)
 
     def get(self, request):
-        return render(request, self.template_name)
+        template = loader.get_template('ticket.html')
+
+        current_user_id = request.user.id
+        myticket = JlsTickets.objects.filter(v_id=current_user_id)
+        ticket_count = JlsTickets.objects.count()
+        context = {
+            'myticket': myticket,
+            'ticket_count': ticket_count,
+        }
+        return HttpResponse(template.render(context, request))
 
     def post(self, request, **kwargs):
         current_user_id = self.request.user.id
-        vdate = request.POST.get('visit date')
-        # if the user didn't choose a date
-        if vdate == "":
-            messages.success(request, "Please select a date to visit")
+        if 'add_ticket' in request.POST:
+            vdate = request.POST.get('visit date')
+            # if the user didn't choose a date
+            if vdate == "":
+                messages.success(request, "Please select a date to visit")
+            else:
+                tk = JlsTickets.objects.filter(v_id=current_user_id, tk_vdate=vdate)
+                # if the user already bought a ticket for that day
+                if len(tk) != 0:
+                    messages.success(request, "You've already bought a ticket!")
+                else: 
+                    visitor = JlsVisitors.objects.get(user_id=current_user_id)
+                    # create a new tickect for this user of the chosen visit date
+                    new_tk = JlsTickets()
+                    new_tk.v_id = visitor.v_id
+                    new_tk.tk_method = 'OL'
+                    new_tk.tk_purdate = timezone.now()
+                    new_tk.tk_vdate = vdate
+                    new_tk.tk_price = 60 # store the original price or price after discount?
+                    new_tk.tk_discount = self.calculateDiscount(new_tk.tk_method, vdate, visitor)
+                    new_tk.save()
+                    messages.success(request, 'Successfully added')
+
+                    # create a new invoice with the ticket, when there's no invoice
+                    temp = JlsInvoi.objects.create(
+                        invoi_date=date.today(),
+                        invoi_amount=self.calculatePrice(new_tk.tk_price, new_tk.tk_discount),
+                        invoi_type='Tickets'
+                    )
+                    temp.save()
+
+                    # use the newly create invoice id to fill in the ticket invoice id
+                    new_tk.invoi_id = temp.invoi_id
+                    new_tk.save()
         else:
-            tk = JlsTickets.objects.filter(v_id=current_user_id, tk_vdate=vdate)
-            # if the user already bought a ticket for that daye
-            if len(tk) != 0:
-                messages.success(request, "You've already bought a ticket!")
-            else: 
-                visitor = JlsVisitors.objects.get(user_id=current_user_id)
-                # create a new tickect for this user of the chosen visit date
-                new_tk = JlsTickets()
-                new_tk.v_id = visitor.v_id
-                new_tk.tk_method = 'OL'
-                new_tk.tk_purdate = timezone.now()
-                new_tk.tk_vdate = vdate
-                new_tk.tk_price = 60 # store the original price or price after discount?
-                new_tk.tk_discount = self.calculateDiscount(new_tk.tk_method, vdate, visitor)
-                new_tk.save()
-                messages.success(request, 'Successfully added')
-
-                # create a new invoice with the ticket, when there's no invoice
-                temp = JlsInvoi.objects.create(
-                    invoi_date=date.today(),
-                    invoi_amount=self.calculatePrice(new_tk.tk_price, new_tk.tk_discount),
-                    invoi_type='Tickets'
-                )
-                temp.save()
-
-                # use the newly create invoice id to fill in the ticket invoice id
-                new_tk.invoi_id = temp.invoi_id
-                new_tk.save()
+            all_tk = JlsTickets.objects.filter(v_id=current_user_id)
+            all_tk.delete()
+            messages.success(request, 'Successfully deleted all tickets')
         return render(request, self.template_name)
