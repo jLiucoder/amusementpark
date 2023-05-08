57decfa904225d377f0e1745bcacca6d5cfbc0e6
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2023-05-08 02:33:57 -0400

    broken cart

--- cart/views.py
+++ cart/views.py
@@ -10,7 +10,7 @@ from visitor.models import JlsVisitors, JlsMember
 from ticket.models import JlsTickets
 from parking.models import JlsParkings
 from .models import JlsInvoi
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect, reverse, render
 
 
 @login_required(login_url='home')
@@ -39,7 +39,8 @@ def cart(request):
     show_totalprice = 0
 
     if len(JlsInvoi.objects.filter(invoi_date=date.today(), jlsvsi__v=visitor, invoi_type='Shows')) > 0:
-        show_totalprice = JlsInvoi.objects.filter(invoi_date=date.today(), jlsvsi__v=visitor, invoi_type='Shows').first().invoi_amount
+        show_totalprice = JlsInvoi.objects.filter(invoi_date=date.today(), jlsvsi__v=visitor,
+                                                  invoi_type='Shows').first().invoi_amount
 
     # parking
     myparking = JlsParkings.objects.filter(v_id=visitor.v_id)
@@ -47,8 +48,6 @@ def cart(request):
 
     # stores
     mystore = JlsOrder.objects.filter(v_id=visitor.v_id)
-
-
 
     context = {
         'mytickets': mytickets,
@@ -66,5 +65,47 @@ def cart(request):
     return HttpResponse(template.render(context, request))
 
 
+@login_required(login_url='home')
+def post(request):
+    template_name = 'cart.html'
+    current_user_id = request.user.id
+    visitor = JlsVisitors.objects.filter(user_id=current_user_id).first()
+    print(visitor.v_id)
+
+    if request.method == 'POST':
+        data = request.POST
+        print(data)
+
+        if 'delete-membership' in request.POST:
+            print('mem')
+            visitor.v_type = 'I'
+            visitor.save()
+            JlsMember.objects.all().delete()
+            visitor.refresh_from_db()
+
+            # return redirect(reverse('cart'))
+
+        elif 'delete-tickets' in request.POST:
+            print('tickets')
+            JlsInvoi.objects.filter(v_id=visitor.v_id, invoi_date=date.today(), invoi_type='Tickets').delete()
+            # for obj in group:
+            #     obj.delete()
+            # return redirect(reverse('cart'))
+
+        elif 'delete-shows' in request.POST:
+            JlsInvoi.objects.filter(v_id=visitor.v_id, invoi_date=date.today(), invoi_type='Shows').delete()
+            # for obj in group:
+            #     obj.delete()
+            # return redirect(reverse('cart'))
+
+        elif 'delete-parkings' in request.POST:
+            JlsInvoi.objects.filter(v_id=visitor.v_id, invoi_date=date.today(), invoi_type='Parkings').delete()
+            # for obj in group:
+            #     obj.delete()
+            # return redirect(reverse('cart'))
+
+    return render(request, template_name)
+
+@login_required(login_url='home')
 def pay(request):
     return redirect(reverse('pay'))
